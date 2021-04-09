48cf907da87fb644c550074c678fc0d44eca7727
fluid-from <81910555+fluid-from@users.noreply.github.com>
2021-04-09 18:47:41 +0900

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,7 +51,7 @@ class Layout extends Component {
     src={ require('../components/sns_y.png') }
     width='20px'
     height='20px'/></a></li>
-    <li><a href="https://twitter.com/fluid_from" target="_blank" rel="noopener noreferrer">
+    <li><a href="http://www.podbbang.com/ch/1780218" target="_blank" rel="noopener noreferrer">
               <img
     src={ require('../components/sns_p.png') }
     width='20px'
